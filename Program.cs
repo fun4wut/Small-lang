8fe7b2584c1b7b72b90ac00bb1e51a1da02e3c51
fun4wut <nihouze@163.com>
2020-08-01 09:56:38 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kumiko_lang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
